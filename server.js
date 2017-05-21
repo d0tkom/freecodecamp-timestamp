4be64da8b75997ca1f40b973df60c516da56968c
Patrik Singer <singerpatrik@gmail.com>
2017-05-21 19:27:58 +0000

    finish

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var path = process.cwd();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(path + '/index.html');
+})
+
+app.get('/:date', function (req, res) {
+    
+    var input = req.params.date;
+    var json;
+    if (isNaN(input)) { // not a number
+        var date = new Date(input);
+        if (date.toDateString() != "Invalid Date") {
+            json = {unix: date.getTime() / 1000, natural: input};
+        }
+        else {
+            json = {unix: null, natural: null};   
+        }
+    }
+    else { // number
+        var time = parseInt(input);
+        // convert seconds to milliseconds
+        time = time * 1000;
+        // try to get date
+        var nat = new Date(time);
+        nat = nat.toDateString();
+        // check if date is valid
+        if (nat == "Invalid Date") {
+            json = {unix: null, natural: null};
+        }
+        else {
+            json = {unix: input, natural: nat};   
+        }
+    }
+    res.json(json);
 })
 
 app.listen(8080, function () {
